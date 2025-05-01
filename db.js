e328bd79301eb8fa26f81c638dc92a6110f7c888
bradyg2000888 <bradygoulden@gmail.com>
2025-05-01 16:02:06 -0400

    Force IPv4 resolution for Supabase connection

--- db.js
+++ db.js
@@ -1,9 +1,12 @@
-
 const { Pool } = require('pg');
+const dns = require('dns');
+dns.setDefaultResultOrder('ipv4first'); // ⬅️ Force IPv4 resolution
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
