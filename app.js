0d516caa293316548b1c748014d43d16445c5f37
bradyg2000888 <bradygoulden@gmail.com>
2025-05-01 13:20:14 -0400

    Add /test route to verify routing

--- app.js
+++ app.js
@@ -12,4 +12,8 @@ app.use(express.json());
 app.use('/api/breaks', breakRoutes);
 
 const PORT = process.env.PORT || 3000;
+app.get('/test', (req, res) => {
+  res.json({ message: 'Test route is working!' });
+});
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
